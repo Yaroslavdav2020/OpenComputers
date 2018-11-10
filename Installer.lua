7f264447c7e8f01b29126e440f6b9aa8ab8baeb0
Yaroslavdav2020 <yaroslavdav2017@yandex.ru>
2018-11-10 17:04:09 +0300

    Update Installer.lua

--- Installer.lua
+++ Installer.lua
@@ -12,10 +12,10 @@ local args, options = shell.parse(...)
 ------------------------------------------------------------------------------------------------------------------------------------
 
 local URLs = {
-	applicationList = "https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/Files.cfg",
-	installerLocalization = "https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/Localizations/Installer/",
-	EFI = "https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/EFI/Minified.lua",
-	license = "https://raw.githubusercontent.com/IgorTimofeev/MineOS/master/LICENSE",
+	applicationList = "https://raw.githubusercontent.com/Yaroslavdav2020/OpenComputers/master/Files.cfg",
+	installerLocalization = "https://raw.githubusercontent.com/Yaroslavdav2020/OpenComputers/master/Localizations/Installer/",
+	EFI = "https://raw.githubusercontent.com/Yaroslavdav2020/OpenComputers/master/EFI/Minified.lua",
+	license = "https://raw.githubusercontent.com/Yaroslavdav2020/OpenComputers/master/LICENSE",
 }
 
 ------------------------------------------------------------------------------------------------------------------------------------
